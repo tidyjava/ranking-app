b584fdb0df9d032ff9c9db855c604ff7a7578cad
Grzegorz Ziemonski <grzegorz.ziemonski@gmail.com>
2017-10-26 09:26:05 +0200

    Part 3: Grand redesign and a big mess

--- src/main/java/com/tidyjava/rankingapp/Ranking.kt
+++ src/main/java/com/tidyjava/rankingapp/Ranking.kt
@@ -4,29 +4,67 @@ import java.time.LocalDateTime
 import java.util.*
 
 class Ranking(val name: String,
-              val defaultRating: Rating,
-              val ratings: MutableMap<Player, Rating> = mutableMapOf(),
+              val defaultRating: Int,
+              val ratings: Set<Rating> = mutableSetOf(),
               val matches: MutableList<Match> = mutableListOf()) {
-
-    fun join(player: Player) {
-        ratings.put(player, defaultRating)
-    }
-
-    fun addMatch(match: Match) {
-        requireRanked(match.player1)
-        requireRanked(match.player2)
-        matches.add(match)
-    }
-
-    private fun requireRanked(player: Player) {
-        if (!ratings.containsKey(player))
-            throw PlayerNotRankedException(player)
-    }
 
     fun confirmResult(id: MatchId) {
         val match = matches.find { it.id == id } ?: throw MatchNotFoundException(id)
         match.status = MatchStatus.CONFIRMED
     }
+
+    fun isRanked(player: Player) = ratings.any { it.player == player }
+}
+
+class RankingService(val ratings: RatingRepository,
+                     val players: PlayerRepository,
+                     val rankings: RankingRepository,
+                     val matches: MatchRepository) {
+
+    fun join(request: JoinRankingRequest) {
+        val player = players.byName(request.player)!!
+        val ranking = rankings.byName(request.ranking)!!
+
+        ratings.add(Rating(player, ranking))
+    }
+
+    fun addMatch(request: AddMatchRequest) {
+        val player1 = players.byName(request.player1)!!
+        val player2 = players.byName(request.player2)!!
+        val ranking = rankings.byName(request.ranking)!!
+
+        matches.add(Match(
+                request.dateTime,
+                player1,
+                player2,
+                ranking,
+                MatchResult.valueOf(request.result)))
+    }
+}
+
+data class JoinRankingRequest(val player: String,
+                              val ranking: String)
+
+data class AddMatchRequest(val dateTime: LocalDateTime,
+                           val player1: String,
+                           val player2: String,
+                           var result: String,
+                           var ranking: String)
+
+interface RatingRepository {
+    fun add(rating: Rating)
+}
+
+interface RankingRepository {
+    fun byName(name: String): Ranking?
+}
+
+interface PlayerRepository {
+    fun byName(name: String): Player?
+}
+
+interface MatchRepository {
+    fun add(match: Match)
 }
 
 class PlayerNotRankedException(player: Player) : RuntimeException("Player $player has not joined this ranking!")
@@ -36,11 +74,20 @@ class MatchNotFoundException(id: MatchId) : RuntimeException("Match $id not foun
 class Match(val dateTime: LocalDateTime,
             val player1: Player,
             val player2: Player,
+            val ranking: Ranking,
             var result: MatchResult,
             var status: MatchStatus = MatchStatus.WAITING_FOR_PLAYER2,
             val id: MatchId = MatchId()) {
 
+    init {
+        requireRanked(player1, ranking)
+        requireRanked(player2, ranking)
+    }
 
+    private fun requireRanked(player: Player, ranking: Ranking) {
+        if (!ranking.isRanked(player))
+            throw PlayerNotRankedException(player)
+    }
 }
 
 data class MatchId(val value: UUID = UUID.randomUUID())
@@ -55,8 +102,11 @@ enum class MatchStatus {
     WAITING_FOR_PLAYER1, WAITING_FOR_PLAYER2, CONFIRMED
 }
 
-class Player {
-
+class Player(val name: String) {
 }
 
-data class Rating(val value: Int)
+class Rating(val player: Player,
+             val ranking: Ranking,
+             val value: Int = ranking.defaultRating) {
+
+}
